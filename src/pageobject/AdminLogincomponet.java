d5afc824c036697d6f2882e322d12723ca64799d
striker <striker@striker>
2018-10-04 18:06:01 +0530

    second

--- src/pageobject/AdminLogincomponet.java
+++ src/pageobject/AdminLogincomponet.java
@@ -1,11 +1,33 @@
 package pageobject;
 
-public class AdminLogincomponet {
-	AdminLogincomponet(){
-	String className=getClass().getName();
-	
-	}
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import ActionDriver.UTF;
+import utilities.ExcelReusables;
+
+public class AdminLogincomponet {
+	public AdminLogincomponet(String filename){
+	//String className=getClass().getName();
+	try {
+		ExcelReusables excelreader=new ExcelReusables( filename);
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	}
+    public void Adminlogin(String sheetnmame) throws Exception {
+    	    String username=ExcelReusables.getCellData(1, 0, sheetnmame);
+			String password=ExcelReusables.getCellData(1, 1, sheetnmame);
+		
+       UTF.clickelement("tagname", "Login");
+       UTF.inputText("id", "username", username);
+       UTF.inputText("id", "password", password);
+    	
+    	
+    	
+    }
+    
 	
 	
 	
--- src/testcases/AdminLogin.java
+++ src/testcases/AdminLogin.java
@@ -6,7 +6,7 @@ import pageobject.AdminLogincomponet;
 public class AdminLogin {
    public static void main(String[] args) throws Exception {
 	   Browser.startBrowser("Chrome", "http://demo.office24by7.com/");
-	   AdminLogincomponet admin=new AdminLogincomponet(System.getProperty("user.dir")+"//Lib//AdminLogin.xlsx"); 
+	   AdminLogincomponet admins=new AdminLogincomponet(System.getProperty("user.dir")+"//Lib//AdminLogin.xlsx"); 
 	 //  admin.Adminlogin("Sheet1");
 	   
 }
