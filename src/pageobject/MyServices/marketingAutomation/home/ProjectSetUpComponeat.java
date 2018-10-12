ded675bd2658f6df6a70dd741657b0d181c622d1
striker <striker@striker>
2018-10-12 18:39:41 +0530

    12 office commit

--- src/pageobject/MyServices/marketingAutomation/home/ProjectSetUpComponeat.java
+++ src/pageobject/MyServices/marketingAutomation/home/ProjectSetUpComponeat.java
@@ -156,8 +156,6 @@ public class ProjectSetUpComponeat {
       Sync.procesWait(2000);
       HomeComponent.clickProjectSetUp();
       Sync.procesWait(2000);
-     // galaxy-topslide
-      
       UTF.clickelement("xpath", "/html/body/div[4]/div/table/tbody/tr/td[6]/a[1]");
       
       }
@@ -167,15 +165,16 @@ public class ProjectSetUpComponeat {
     	Log4j.infoLog("click on the ProjectManagement_upperbord");
     	
     }
-    public static void clickOn_ProjectManagement_delete() {
+    public  void clickOn_ProjectManagement_delete() {
     	 Sync.procesWait(3000);
 	   	 UTF.clickelement("xpath", "//li[@title='Project Management']");
 	     Sync.procesWait(2000);
-	     UTF.clickelement("cssSelector", ".btn.btn-danger.glxy-delet");
-	     Log4j.infoLog("delete button clicked ");
+	     UTF.clickelement("xpath", ".//*[@id='MARKETING_PROJECT_MANAGEMENT']/following::button[1]");
+    	 Log4j.infoLog("delete button clicked ");
 	   	 Sync.procesWait(3000);
 	   	 UTF.clickelement("partialLinkText", "Yes");
-    	 Log4j.infoLog("delete ProjectManagement" );
+  	     Log4j.infoLog("delete ProjectManagement" );
+  	    
     }
 	public static void clickOn_projuctManagement_lowerbord() {
 		Sync.procesWait(3000);
@@ -189,7 +188,7 @@ public class ProjectSetUpComponeat {
     }
     public static void ClickOn__userprivilleges_lowerbord() {
     	Sync.procesWait(3000);
-    	UTF.clickelement("xpath", ".//*[@id='1' ]/div");
+    	 UTF.clickelement("xpath", "//*[contains(@title, 'UserPrivilleges')]");
     	Log4j.infoLog("ClickOn__userprivilleges_lowerbord");
     }
     
@@ -197,12 +196,11 @@ public class ProjectSetUpComponeat {
 	
      public static void ClickOn_userPrivilleges_Editoption() {
     	 Sync.procesWait(3000);
-    	 //List<WebElement> elements=UTF.findelements("xpath", ".//*[@id='MARKETING_USER_ROLES']");
-    	// UTF.mouseover("xpath", "//*[contains(@title, 'edit')]");
+    	 UTF.clickelement("xpath", "//*[contains(@title, 'User Privilleges')]");
+    	 Sync.procesWait(3000);          
     	UTF.clickelement("xpath", ".//*[@id='MARKETING_USER_ROLES' and  @type='button']");
-    	 Sync.procesWait(3000);
-    	//String s=UTF.findElement("cssSelector", ".side-frame-head").getText();
-       // System.out.println(s);
+    	
+    	Sync.procesWait(3000);
     	Log4j.infoLog("ClickOn_userPrivilleges_Editoption");
      }
      public static void clickOn_USERS_Upperbord() {
@@ -216,6 +214,15 @@ public class ProjectSetUpComponeat {
     	 UTF.clickelement("xpath", "//*[contains(@title, 'Users')]");
     	 Log4j.infoLog("clickOn_User_Lowerbord");
      }
+     public void clickOn_user_delete() {
+    	 Sync.procesWait(3000);
+    	 UTF.clickelement("xpath", ".//*[@id='MARKETING_USERS']/following::button[1]");
+    	 Log4j.infoLog("delete button clicked ");
+	   	 Sync.procesWait(3000);
+	   	 UTF.clickelement("partialLinkText", "Yes");
+  	     Log4j.infoLog("delete user" );
+     }
+     
      public static void clickOn_CXO_Upperbord() {
     	 Sync.procesWait(3000);
     	 UTF.clickelement("id", "append_values_13,append_values_19,MARKETING_CXO");
@@ -233,6 +240,15 @@ public class ProjectSetUpComponeat {
     	 UTF.clickelement("xpath", ".//*[@id='MARKETING_CXO' and @type='button']");
     	 Log4j.infoLog("clickOn_CXO_Editoption");
     	
+    }
+    public void delete_CXO() {
+    	 Sync.procesWait(3000);
+   	     UTF.clickelement("xpath", "//*[contains(@title, 'CXO')]");
+    	 UTF.clickelement("xpath", ".//*[@id='MARKETING_CXO']/following::button[2]");
+   	     Log4j.infoLog("delete button clicked ");
+   	   	 Sync.procesWait(3000);
+   	   	 UTF.clickelement("partialLinkText", "Yes");
+     	 Log4j.infoLog("delete_CXO" );
     }
     public static void clickOn_Manager_Upperbord() {
    	 Sync.procesWait(3000);
@@ -256,67 +272,197 @@ public class ProjectSetUpComponeat {
 	     Sync.procesWait(3000);
 	     UTF.mouseover("xpath", "//li[@title='Manager']");
 	     Log4j.infoLog("clickOn_Manager_Lowerbord");
-	     Sync.procesWait(4000);             
-	     UTF.clickelement("xpath", ".//li[@title='Manager']/following::button[2]");
+	     Sync.procesWait(4000);    
+	     UTF.clickelement("xpath", ".//*[@id='MARKETING_USER_MANAGER']/following::button[2]");
+	  // It is usefull for upper menu> UTF.clickelement("xpath", ".//*[@id='MARKETING_USER_MANAGER' and @type='button']");
 	     Log4j.infoLog("delete button clicked ");
 	   	 Sync.procesWait(3000);
 	   	 UTF.clickelement("partialLinkText", "Yes");
-  	     Log4j.infoLog("delete ProjectManagement" );
+  	     Log4j.infoLog("delete_Manager" );
 	   
 	   }
    
    public static void clickOn_Template_Settings_Upperbord() {
 		Sync.procesWait(3000);
 		UTF.clickelement("id", "append_values_2,append_values_14,MARKETING_PROJECT_SETTINGS");
-		Log4j.infoLog("click On Template_Settings_Upperbord");
+		Log4j.infoLog("click On Template_Settings_upper");
 	}
      public static void ClickOn_Template_Settings_Lowerbord() {
    	 Sync.procesWait(3000);
      UTF.clickelement("xpath", "//*[contains(@title, 'Template Settings')]");
  	 Log4j.infoLog(" ClickOn_Template_Settings_Lowerbord");
     }
-    public static void clickOn_SMS_Upperbord() {
-		Sync.procesWait(3000);
-	    UTF.clickelement("id", "append_values_14,append_values_24,MARKETING_SMS_STORE");
-		Log4j.infoLog("clickOn_SMS_Upperbord");
+    public void delete_Template_Settings() { 
+    	Sync.procesWait(3000);
+    	UTF.clickelement("xpath", "//*[contains(@title, 'Template Settings')]");
+        Sync.procesWait(4000);    
+ 	    UTF.clickelement("xpath", ".//*[@id='MARKETING_PROJECT_SETTINGS']/following::button[1]");
+ 	     Sync.procesWait(2000);
+	   	 UTF.clickelement("partialLinkText", "Yes");
+ 	    Log4j.infoLog(" Detele_Template_Settings");
+    }
+  // MARKETING_SMS_STORE
+     
+     
+     
+      public static void clickOn_SMS_Upperbord() {
+		   Sync.procesWait(3000);
+	       UTF.clickelement("id", "append_values_14,append_values_24,MARKETING_SMS_STORE");
+		   Log4j.infoLog("clickOn_SMS_Upperbord");
 	  }
-      public static void ClickOn_SMS_Lowerbord() {
- 	   Sync.procesWait(3000);
-       UTF.clickelement("xpath", "//*[contains(@title, 'SMS')]");
-	   Log4j.infoLog("ClickOn_SMS_Lowerbord");
+       public static void ClickOn_SMS_Lowerbord() {
+ 	       Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'SMS')]");
+	       Log4j.infoLog("ClickOn_SMS_Lowerbord");
         }
-      
+       public void delete_SMSStore() {
+    	   Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'SMS')]");
+           Sync.procesWait(4000);    
+   	       UTF.clickelement("xpath", ".//*[@id='MARKETING_SMS_STORE']/following::button[1]");
+   	       Sync.procesWait(2000);
+	   	   UTF.clickelement("partialLinkText", "Yes");
+   	       Log4j.infoLog(" Detele_SMSStore");
+      }
+       
+       
+       public void clickOn_MARKETING_SMS_TEMPLETES_Upperbord() {
+    	   Sync.procesWait(3000);
+	       UTF.clickelement("id", "append_values_24,append_values_31,MARKETING_SMS_TEMPLETES");
+		   Log4j.infoLog("clickOn_MARKETING_SMS_TEMPLETES_Upperbord");
+    	   
+       }
+       public static void ClickOn_MARKETING_SMS_TEMPLETES_Lowerbord() {
+	       Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'SMS Templates')]");
+	       Log4j.infoLog("ClickOn_SENDER_NAMES_Lowerbord");
+       }
+       public static void clickOn_MARKETING_SMS_TEMPLETES_Editoption() {
+ 	      Sync.procesWait(3000);
+   	      UTF.clickelement("xpath", ".//*[@id='MARKETING_SMS_TEMPLETES' and @type='button']");
+   	      Log4j.infoLog("clickOn_SENDER_NAMES_Editoption");
+   	      Sync.procesWait(2000);
+         }
+       public void delete_MARKETING_SMS_TEMPLETES() {
+    	   Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'SMS Templates')]");
+           Sync.procesWait(2000);    
+   	       UTF.clickelement("xpath", ".//*[@id='MARKETING_SMS_TEMPLETES']/following::button[2]");
+   	       Sync.procesWait(2000);
+	   	   UTF.clickelement("partialLinkText", "Yes");
+   	       Log4j.infoLog("delete_MARKETING_SMS_TEMPLETES");
+      }
+       
+       public void clickOn_SENDER_NAMES_Upperbord() {
+    	   Sync.procesWait(3000);
+	       UTF.clickelement("id", "append_values_24,append_values_30,MARKETING_SENDER_NAMES");
+		   Log4j.infoLog("SENDER_NAMES_Upperbord");
+    	   
+       }
+       public  void ClickOn_SENDER_NAMES_Lowerbord() {
+	       Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'Sender Names')]");
+	       Log4j.infoLog("ClickOn_SENDER_NAMES_Lowerbord");
+       }
+       public  void clickOn_SENDER_NAMES_Editoption() {
+ 	      Sync.procesWait(3000);
+   	      UTF.clickelement("xpath", ".//*[@id='MARKETING_SENDER_NAMES' and @type='button']");
+   	      Log4j.infoLog("clickOn_SENDER_NAMES_Editoption");
+   	      Sync.procesWait(2000);
+         }
+       public void delete_SENDER_NAMES() {
+    	   Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'Sender Names')]");
+           Sync.procesWait(2000);    
+   	       UTF.clickelement("xpath", ".//*[@id='MARKETING_SENDER_NAMES']/following::button[2]");
+   	       Sync.procesWait(2000);
+	   	   UTF.clickelement("partialLinkText", "Yes");
+   	       Log4j.infoLog(" Detele_SENDER_NAMES");
+      }
+       
+       
+       
        public static void clickOn_Email_Upperbord() {
-		Sync.procesWait(3000);
-	    UTF.clickelement("id", "append_values_14,append_values_25,MARKETING_EMAIL_STORE");
-		Log4j.infoLog("clickOn_Email_Upperbord");
+		   Sync.procesWait(3000);
+	       UTF.clickelement("id", "append_values_14,append_values_25,MARKETING_EMAIL_STORE");
+		   Log4j.infoLog("clickOn_Email_Upperbord");
 	   }
        public static void ClickOn_Email_Lowerbord() {
-	   Sync.procesWait(3000);
-       UTF.clickelement("xpath", "//*[contains(@title, 'Email')]");
-	   Log4j.infoLog("ClickOn_Email_Lowerbord");
+	       Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'Email')]");
+	       Log4j.infoLog("ClickOn_Email_Lowerbord");
        }
+       public void dalete_EmailStore() {
+    	   Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'Email')]");
+           Sync.procesWait(4000);    
+    	   UTF.clickelement("xpath", ".//*[@id='MARKETING_EMAIL_STORE']/following::button[1]");
+    	   Sync.procesWait(2000);
+	   	   UTF.clickelement("partialLinkText", "Yes");
+    	   Log4j.infoLog(" EmailStore");
+       }
+       
+       public static void clickOn_MARKETING_EMAIL_SETTINGS_Upperbord() {
+	      Sync.procesWait(3000);
+          UTF.clickelement("id", "append_values_25,append_values_32,MARKETING_EMAIL_SETTINGS");
+	      Log4j.infoLog("clickOn_MARKETING_EMAIL_SETTINGS_Upperbord");
+       }
+       public static void ClickOn_MARKETING_EMAIL_SETTINGS_Lowerbord() {
+          Sync.procesWait(3000);
+          UTF.clickelement("xpath", "//*[contains(@title, 'Email Settings')]");
+          Log4j.infoLog("ClickOn_MARKETING_EMAIL_SETTINGS_Lowerbord");
+        }
+
+
+       public static void clickOn_MARKETING_EMAIL_SETTINGS_Editoption() throws Exception {
+	      Sync.procesWait(3000);
+  	      UTF.clickelement("xpath", ".//*[@id='MARKETING_EMAIL_SETTINGS' and @type='button']");
+  	      Log4j.infoLog("clickOn_MARKETING_EMAIL_SETTINGS_Editoption");
+  	      Sync.procesWait(2000);
+        }
+       
+       public void delete_MARKETING_EMAIL_SETTINGS() {
+    	   Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'Email Settings')]");
+           Sync.procesWait(2000);    
+    	   UTF.clickelement("xpath", ".//*[@id='MARKETING_EMAIL_SETTINGS']/following::button[2]");
+    	   Sync.procesWait(2000);
+	   	   UTF.clickelement("partialLinkText", "Yes");
+    	   Log4j.infoLog("delete_MARKETING_EMAIL_SETTINGS");
+    	   
+       } 
+       
+       
        public static void clickOn_Audio_Upperbord() {
-	    Sync.procesWait(3000);
-        UTF.clickelement("id", "append_values_14,append_values_26,MARKETING_AUDIO_STORE");
-	    Log4j.infoLog("clickOn_Audio_Upperbord");
+	      Sync.procesWait(3000);
+          UTF.clickelement("id", "append_values_14,append_values_26,MARKETING_AUDIO_STORE");
+	      Log4j.infoLog("clickOn_Audio_Upperbord");
        }
-        public static void ClickOn_Audio_Lowerbord() {
-        Sync.procesWait(3000);
-        UTF.clickelement("xpath", "//*[contains(@title, 'Audio')]");
-        Log4j.infoLog("ClickOn_Audio_Lowerbord");
+       public static void ClickOn_Audio_Lowerbord() {
+          Sync.procesWait(3000);
+          UTF.clickelement("xpath", "//*[contains(@title, 'Audio')]");
+          Log4j.infoLog("ClickOn_Audio_Lowerbord");
         }
 
 
        public static void clickOn_Audio_Editoption() throws Exception {
-	    Sync.procesWait(3000);
-  	    UTF.clickelement("xpath", ".//*[@id='MARKETING_AUDIO_STORE' and @type='button']");
-  	    Log4j.infoLog("clickOn_Audio_Editoption");
-  	    Sync.procesWait(2000);
-   }
-  
-
-
+	      Sync.procesWait(3000);
+  	      UTF.clickelement("xpath", ".//*[@id='MARKETING_AUDIO_STORE' and @type='button']");
+  	      Log4j.infoLog("clickOn_Audio_Editoption");
+  	      Sync.procesWait(2000);
+        }
+       
+       public void delete_AudioStore() {
+    	   Sync.procesWait(3000);
+           UTF.clickelement("xpath", "//*[contains(@title, 'Audio')]");
+           Sync.procesWait(4000);    
+    	   UTF.clickelement("xpath", ".//*[@id='MARKETING_AUDIO_STORE']/following::button[2]");
+    	   Sync.procesWait(2000);
+	   	   UTF.clickelement("partialLinkText", "Yes");
+    	   Log4j.infoLog(" EmailStore");
+    	   
+       }
+       
 
 
 
@@ -423,6 +569,10 @@ public class ProjectSetUpComponeat {
 	     UTF.selectByVisibleText("id", "Dept_ID", AddUserRoles);
 	     Log4j.infoLog("select the add user role");
 	     
+	     Sync.procesWait(2000);
+	     UTF.selectByVisibleText("id", "Reporting_Dept_UserId", SelectReportingRole);
+	    Log4j.infoLog("select the deporting");
+	     
 	     UTF.selectByValue("id", "countryid", Country);
 	     Log4j.infoLog("select the Country");
 	     UTF.selectByVisibleText("id", "stateid",state );
@@ -439,11 +589,11 @@ public class ProjectSetUpComponeat {
 	     
 	     UTF.selectByVisibleText("className", "ui-datepicker-month", JoiningDateMonth);
 	     Log4j.infoLog("select JoiningDateManth ");
-	     UTF.clickelement("partialLinkText", "6");
-	     Log4j.infoLog("select DAY ");
+	    
 	     UTF.selectByValue("className", "ui-datepicker-year", JoiningDateyear);
 	     Log4j.infoLog("select JoiningDateyear ");
-	     
+	     UTF.clickelement("partialLinkText", "6");
+	     Log4j.infoLog("select DAY ");
 	     UTF.inputText("id", "Emp_ID", EmployeeID);
 	     Log4j.infoLog("EmployeeID ");
 	  
@@ -452,16 +602,21 @@ public class ProjectSetUpComponeat {
 	     Sync.procesWait(2000);
 	     UTF.selectByVisibleText("className", "ui-datepicker-month", DateOfBirthmonth);
 	     Log4j.infoLog("select DateOfBirth month ");
-	     UTF.clickelement("partialLinkText", "6");
-	     Log4j.infoLog("Day selected ");
+	    
+	     Sync.procesWait(2000);
 	     UTF.selectByValue("className", "ui-datepicker-year", DateOfBirthyear);
 	     Log4j.infoLog("select DateOfBirthyear ");
-	     UTF.clickelement("xpath", ".//*[@id='adduser']/div[21]/button");
+	     UTF.clickelement("partialLinkText", "6");
+	     Log4j.infoLog("Day selected ");
+	     Sync.procesWait(2000);
+	     UTF.clickelement("id", "usersubmit");
+	     Log4j.infoLog("click submit ");
 	     
+	     /*UTF.clickelement("xpath", ".//*[@id='adduser']/div[21]/button");
+	     Log4j.infoLog("click the cancile ");
+	     */
 	     
-	   /*  Assert.assertTrue(message.equals("NO DATA"));
-	 	 UTF.SwitchToDefaltFrame();
-	    */
+	  
    }
    
        public void addCXOfields() throws Exception {
@@ -605,8 +760,40 @@ public class ProjectSetUpComponeat {
 	   
        }
    
+      public void checking_manager_usersRoles() {
+    	  Sync.procesWait(2000);
+    	  UTF.SwitchToFrameByWebElement("id", "Dynamic_Frame");
+   	      Sync.procesWait(2000);
+   	   UTF.clickelement("xpath", ".//*[@id='item_127']/tr[1]/td[5]/button");
+   	  Sync.procesWait(2000);
+   	  List<WebElement> elements=UTF.findelements("xpath", ".//*[@id='item_127']/tr/td/h4/span");
+   	      for(WebElement text:elements){
+   	    	   System.out.println(text.getText());
+   	      }
+   	      
+   	   	  Sync.procesWait(2000);
+   	    UTF.clickelement("partialLinkText", "Update");  
+   	      
+   	      
+   	  // UTF.clickelement("xpath", ".//*[@id='item_127']/tr[1]/td[5]/button");
+   	      
+   	      
+   	     // UTF.clickelement("xpath", ".//*[@id='item_127']/tr[1]/td[5]/button");
+   	      
+      }
    
    
+       public void user_Privileges_Web() {
+    	  Sync.procesWait(2000);
+     	  UTF.SwitchToFrameByWebElement("id", "Dynamic_Frame");
+     	  Log4j.infoLog("shfiting uberprivilegs sheet");
+     	  UTF.clickelement("xpath`", ".//*[@id='usertab_privilleges']");
+     	  Sync.procesWait(2000);
+     	  UTF.selectByVisibleText("id", "userrole", "Manager");
+     	  Sync.procesWait(2000);
+     	  UTF.clickelement("linkText", "manoj kumar");
+    	   
+       }
    
    
    
--- src/testCases/Home/ProjectSetUp/Sample.java
+++ src/testCases/Home/ProjectSetUp/Sample.java
@@ -12,16 +12,72 @@ public class Sample {
 	
 		@Test
 		
-		  public void projuctSet_validation () throws Exception {
-			ProjectSetUpComponeat project=new ProjectSetUpComponeat("MyServices/marketingAutomation/home/projuctSetUp/CXO.xml","Verifying_projuctSet_validation");
+		   public void projuctSet_validation () throws Exception {
+			ProjectSetUpComponeat project=new ProjectSetUpComponeat("MyServices/marketingAutomation/home/projuctSetUp/Manager.xml","Verifying_projuctSet_validation");
 			try {
+				
 				project.clickFlowEdit();
+				
 				/*project.clickOn_ProjectManagement_upperbord();
 				project.clickOn_projuctManagement_lowerbord();
-				project.clickOn_Template_Settings_Upperbord();
-				project.ClickOn_Template_Settings_Lowerbord();*/
-				project.delete_Manager();
+				project.ClickOn__userprivilleges_Upperbord();
+				project.ClickOn__userprivilleges_lowerbord();
 				
+				project.ClickOn_userPrivilleges_Editoption();
+				project.user_Privileges_Web();
+				*/
+				
+				
+				project.clickOn_ProjectManagement_upperbord();
+				project.clickOn_projuctManagement_lowerbord();
+				project.clickOn_USERS_Upperbord();
+				project.clickOn_User_Lowerbord();
+				project.clickOn_Manager_Upperbord();
+				project.clickOn_Manager_Lowerbord();
+				project.clickOn_Manager_Editoption();
+				project.checking_manager_usersRoles();
+				
+				
+				
+				
+				
+				/*project.clickFlowEdit();
+				project.clickOn_ProjectManagement_upperbord();
+				project.clickOn_projuctManagement_lowerbord();
+		
+				project.clickOn_Template_Settings_Upperbord();
+                project.ClickOn_Template_Settings_Lowerbord();
+                project.clickOn_SMS_Upperbord();
+                project.ClickOn_SMS_Lowerbord();
+                
+                project.clickOn_SENDER_NAMES_Upperbord();
+                project.ClickOn_SENDER_NAMES_Lowerbord();
+                project.clickOn_SENDER_NAMES_Editoption();
+                project.delete_SENDER_NAMES();
+                
+                project.clickOn_MARKETING_SMS_TEMPLETES_Upperbord();
+                project.ClickOn_MARKETING_SMS_TEMPLETES_Lowerbord();
+                project.clickOn_MARKETING_SMS_TEMPLETES_Editoption();
+                project.delete_MARKETING_SMS_TEMPLETES();
+                
+                project.delete_SMSStore();
+                
+        
+                project.clickOn_Email_Upperbord();
+                project.ClickOn_Email_Lowerbord();
+                project.clickOn_MARKETING_EMAIL_SETTINGS_Upperbord();
+                project.ClickOn_MARKETING_EMAIL_SETTINGS_Lowerbord();
+                project.clickOn_MARKETING_EMAIL_SETTINGS_Editoption();
+                project.delete_MARKETING_EMAIL_SETTINGS();
+                
+                project.dalete_EmailStore();
+                
+                project.clickOn_Audio_Upperbord();
+                project.ClickOn_Audio_Lowerbord();
+                project.clickOn_Audio_Editoption();
+                project.delete_AudioStore();
+                project.delete_Template_Settings();
+			    project.clickOn_ProjectManagement_delete();
 				
 				
 				
